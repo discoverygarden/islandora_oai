431b06fab467780a4bd7e3f8b7f99e75c425eb03
Adam Vessey <adam-vessey@users.noreply.github.com>
2019-05-21 14:11:31 -0300

    Move things over to a controller.

---
+++ src/Controller/Oai.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\islandora_oai\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Route;
+
+/**
+ * OAI controller.
+ */
+class Oai {
+
+  /**
+   * Get OAI response.
+   */
+  public function response() {
+    module_load_include('inc', 'islandora_oai', 'includes/request');
+    $output = islandora_oai_parse_request();
+    $response = new Response();
+    if ($output) {
+      $response->setContent($output);
+    }
+    $response->headers->set('Content-Type', 'text/xml');
+    return $response;
+  }
+
+  /**
+   * Dynamic routing for OAI endpoint.
+   */
+  public static function routes() {
+    $items = [];
+
+    $items['islandora_oai_oai'] = new Route(
+      \Drupal::config('islandora_oai.settings')->get('islandora_oai_path'),
+      [
+        '_title' => 'OAI2',
+        '_controller' => __CLASS__ . '::response',
+      ],
+      ['_permission' => 'access content']
+    );
+
+    return $items;
+  }
+
+}
